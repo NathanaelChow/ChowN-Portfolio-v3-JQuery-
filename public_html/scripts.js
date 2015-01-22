37cef06ea6a555bbb994efa6bf88b8a6629f2cd4
NathanaelChow <nhchowder@gmail.com>
2015-01-22 09:08:17 -0800

    Added flippable textbox, a background video, and added a bunch of comments.
    Also added JQuery to portfolio technologies.

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -36,77 +36,78 @@ $("document").ready(function(){
 
 });
 
-
+//animation that hides the page
 function hideThePage(){
     $('#show').css('visibility', 'visible');
     $('div').hide('slide', {}, 2500);
     $('#show').show('fold', {}, 2500);
 }
-
+//animation that shows the page
 function showThePage(){
     $('div').show('fold', {}, 2500);
     $('#show').hide('puff', {}, 2500);
 
 }
-
+//replaces text through html
 function replaceWHtml(){
     $('#Thing2').html('<h4>Thing 2</h4>');
 }
 
-
+//adds text on command
 function addAPara(){
     $('#randPara').append('<p>Another paragraph</p>');
 }
-
+//removes text that was added
 function removeAPara(){
     $('#randPara p:last').remove();
 }
 
-
+//pops up an alert
 function alertButtonClick() {
 alert("There was a button clicked");
 }
-
+//shows that you left the text box in the events triggered
 function onBlurEvent()
 {
 $("#second").html("You left the text box");
 }
-
+//shows that you entered the text box
 function onFocusEvent()
 {
 $("#second").html("You entered the text box");
 }
-
+//also shows that you left the text box
 function onMDownEvent()
 {
 $("#second").html("You left the text box");
 }
-
+//shows that you entered the text box
 function onMUpEvent()
 {
 $("#second").html("You entered the text box");
 }
-
+//shows when you add text to the text box
 function onChangeEvent()
 {
 $("#third").html("You changed the text box");
 }
-
+//resizes the window
 function resizedWindow()
 {
 $("#second").html("Window was resized W: " + $(window).width() + " H: " + $(window).height());
 }
-
+//when the cursor hovers over the title the background color
+//changes to green
 function mouseOver()
 {
 $(".Thing").css("background-color", "green");
 }
-
+//when the mouse is off the title the background color changes tored
 function mouseOut()
 {
 $(".Thing").css("background-color", "red");
 }
-
+//shows that you clicked the button twice
 function dblClickedMe()
 {
 $("#second").html("You double clicked this button");
